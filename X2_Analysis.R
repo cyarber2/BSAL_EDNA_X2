cdfd1dbcf14579c284f8e08c016e281eeca9ab9a
Jesse Brunner <jesse.brunner@wsu.edu>
2019-02-13 13:51:38 -0500

    Added in a couple of plots of data Added regressions of EstimatedCopiesAdj ~ FilterVolume with the expectation that the slope should be one (i.e., double the volume filtered, double the amount recovered) and made a plot of the estimates and 95%CIs

--- X2_Analysis.R
+++ X2_Analysis.R
@@ -2,7 +2,7 @@ rm(list=ls())
 setwd("~/Desktop/MSResearch/BSAL_EDNA_X2")
 
 library("readxl")
-# Read in raw data from excel file 16 
+# Read in raw data from excel file 16
 Plate_16_raw <- read_excel(path="Bsal_Plate_16_12152018_Resave.xls",
                            sheet=1,
                            skip=19)
@@ -31,9 +31,9 @@ P17$Sample <- gsub(pattern="X2.", replacement="", x=P17$Sample)
 Bsal <- rbind(P16, P17)
 
 # Aggregate by sample
-Bsal.agg <- aggregate(x=Bsal$Starting_Quantity, 
-                      by=list(Sample=Bsal$Sample), 
-                      FUN=mean, 
+Bsal.agg <- aggregate(x=Bsal$Starting_Quantity,
+                      by=list(Sample=Bsal$Sample),
+                      FUN=mean,
                       na.rm=T)
 colnames(Bsal.agg)[2] <- "EstimatedCopies"
 
@@ -42,29 +42,54 @@ BsalPos <- Bsal.agg[-c(1:7, 10, 13, 15, 17, 19, 22, 24, 29, 33, 37, 42), ]
 
 # Adjust estimated copies for dilutions
 diluted.index <- grepl(pattern="D", x=BsalPos$Sample)
-BsalPos$EstimatedCopiesAdj <- 
+BsalPos$EstimatedCopiesAdj <-
   ifelse(grepl(pattern="D", x=BsalPos$Sample),
          BsalPos$EstimatedCopies*10,
          BsalPos$EstimatedCopies)
 BsalPos$EstimatedCopies <- NULL
-  
+
 # Extract info from sample name and prune for filter data only
 sample.expanded <- strsplit(BsalPos$Sample, ".", fixed=T)
-BsalPos$FilterVolume <- sapply(sample.expanded,function(x) x[2]) 
+BsalPos$FilterVolume <- sapply(sample.expanded,function(x) x[2])
 BsalPos$ReplicateContainer <- sapply(sample.expanded,function(x) x[1])
-BsalPos$ReplicateContainer <- 
+BsalPos$ReplicateContainer <-
   gsub(x=BsalPos$ReplicateContainer, pattern="F", replacement="")
-   
-plot(x=BsalPos$FilterVolume, 
-     y=BsalPos$EstimatedCopiesAdj, 
-     #xlim=c(0, 1000), 
-     ylim=c(0, 4.3e6), 
+
+plot(x=BsalPos$FilterVolume,
+     y=BsalPos$EstimatedCopiesAdj,
+     #xlim=c(0, 1000),
+     ylim=c(0, 4.3e6),
      xlab="Volume of Water Filtered",
      ylab="Estimated Copies")
 
-plot(x=BsalPos$ReplicateContainer, 
-     y=BsalPos$EstimatedCopiesAdj, 
-     #xlim=c(0, 1000), 
-     ylim=c(0, 4.3e6), 
+plot(x=BsalPos$ReplicateContainer,
+     y=BsalPos$EstimatedCopiesAdj,
+     #xlim=c(0, 1000),
+     ylim=c(0, 4.3e6),
      xlab="Replicate Container",
      ylab="Estimated Copies")
+
+# Some alternate plots
+library(tidyverse)
+library(broom)
+
+qplot(as.numeric(FilterVolume), EstimatedCopiesAdj, color=ReplicateContainer, data=BsalPos, geom="point") +
+  geom_smooth(method = "lm", se=F) +
+  facet_grid(ifelse(ReplicateContainer==1,"Y","N") ~ ., space="free_y", scales = "free_y")
+
+qplot(as.numeric(FilterVolume), EstimatedCopiesAdj,
+      color=ReplicateContainer,
+      data=subset(BsalPos, ReplicateContainer != 1),
+      geom=c("point", "line")) +
+  geom_smooth(method = "lm", se=F)
+
+# linear regression by container
+lms <- BsalPos %>%
+  group_by(ReplicateContainer) %>%
+  do(mod = lm(EstimatedCopiesAdj ~ as.numeric(FilterVolume), data = .)) %>%
+  tidy(mod, conf.int=TRUE)
+
+ggplot(lms, aes(ReplicateContainer, y = estimate, ymin=conf.low, ymax=conf.high)) +
+  geom_pointrange() +
+  facet_grid(term ~ ., scale="free_y")
+
